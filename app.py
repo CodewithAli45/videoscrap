11af00d00d7095a329057fc5e69e33aa952cc6e2
Ali Murtaza <ali.my0304@outlook.com>
2025-09-20 11:33:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ app.register_blueprint(main_bp)
 app.register_blueprint(api_bp, url_prefix='/api') # Prefix all API routes with /api
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
